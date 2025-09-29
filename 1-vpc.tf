c4263ef6521ca6dab6bbcc7991534513f349c8e3
Vitor Soller <vgsoller@gmail.com>
2025-09-29 09:18:43 -0300

    add initial files from terraform with phase 2 done

---
+++ 1-vpc.tf
@@ -0,0 +1,113 @@
+resource "aws_vpc" "main_vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name   = "project-webapp-vpc"
+    ORIGIN = "TERRAFORM"
+  }
+}
+
+
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name   = "project-webapp-public-subnet-1"
+    ORIGIN = "TERRAFORM"
+  }
+}
+
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name   = "project-webapp-private-subnet-1"
+    ORIGIN = "TERRAFORM"
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name   = "project-webapp-public-subnet-2"
+    ORIGIN = "TERRAFORM"
+  }
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name   = "project-webapp-private-subnet-2"
+    ORIGIN = "TERRAFORM"
+  }
+}
+
+resource "aws_internet_gateway" "main_igw" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name   = "project-webapp-igw"
+    ORIGIN = "TERRAFORM"
+  }
+
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main_igw.id
+  }
+  tags = {
+    Name   = "project-webapp-public-rt"
+    ORIGIN = "TERRAFORM"
+  }
+}
+
+resource "aws_route_table_association" "public_rt_assoc_1" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+resource "aws_route_table_association" "public_rt_assoc_2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table" "private_rt_1" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name   = "project-webapp-private-rt-1"
+    ORIGIN = "TERRAFORM"
+  }
+}
+
+resource "aws_route_table_association" "private_rt_assoc_1" {
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.private_rt_1.id
+}
+
+resource "aws_route_table" "private_rt_2" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name   = "project-webapp-private-rt-2"
+    ORIGIN = "TERRAFORM"
+  }
+}
+
+resource "aws_route_table_association" "private_rt_assoc_2" {
+  subnet_id      = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.private_rt_2.id
+}
---
+++ 2-ec2.tf
@@ -0,0 +1,71 @@
+resource "aws_security_group" "web_sg" {
+  name        = "project-webapp-web-sg"
+  description = "Security group for web server via terraform"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  tags = {
+    Name   = "project-webapp-web-sg"
+    ORIGIN = "TERRAFORM"
+  }
+
+}
+
+resource "aws_security_group_rule" "allow_http_inbound" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.web_sg.id
+  description       = "Allow HTTP inbound traffic"
+
+}
+
+resource "aws_security_group_rule" "allow_https_inbound" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.web_sg.id
+  description       = "Allow HTTPS inbound traffic"
+
+}
+
+resource "aws_security_group_rule" "allow_all_outbound" {
+  type      = "egress"
+  from_port = 0
+  to_port   = 0
+  protocol  = "-1"
+  cidr_blocks = [
+    "0.0.0.0/0"
+  ]
+  security_group_id = aws_security_group.web_sg.id
+  description       = "Allow all outbound traffic"
+}
+
+resource "aws_security_group_rule" "allow_ssh_inbound" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.web_sg.id
+  description       = "Allow SSH inbound traffic"
+
+}
+
+
+resource "aws_instance" "web_server_1" {
+  ami                         = "ami-0360c520857e3138f" # Ubuntu Server 20.04 LTS (HVM), SSD Volume Type
+  instance_type               = "t3.small"
+  subnet_id                   = aws_subnet.public_subnet_1.id
+  vpc_security_group_ids      = [aws_security_group.web_sg.id]
+  associate_public_ip_address = true
+  user_data                   = file("${path.module}/UserdataScript-phase-2.sh")
+
+  tags = {
+    Name   = "project-webapp-web-server-1"
+    ORIGIN = "TERRAFORM"
+  }
+}
