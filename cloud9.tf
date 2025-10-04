8566c46b02df927b662cf8d3d0b074d2d453460d
Vitor Soller <vgsoller@gmail.com>
2025-10-03 23:08:38 -0300

    add cloud9 and rules

--- cloud9.tf
+++ cloud9.tf
@@ -1,8 +1,30 @@
+# Cloud9 Security Group
+resource "aws_security_group" "cloud9_sg" {
+  name        = "project-webapp-cloud9-sg"
+  description = "Security group for Cloud9 environment"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "project-webapp-cloud9-sg"
+  }
+}
+
+resource "aws_security_group_rule" "cloud9_outbound" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.cloud9_sg.id
+  description       = "Allow all outbound traffic"
+}
+
 resource "aws_cloud9_environment_ec2" "cloud9_env" {
   instance_type               = "t3.micro"
   name                        = "project-webapp-cloud9-env"
   image_id                    = "amazonlinux-2023-x86_64"
   subnet_id                   = aws_subnet.public_subnet_1.id
   automatic_stop_time_minutes = 30
+
 
 }
--- ec2.tf
+++ ec2.tf
@@ -43,20 +43,19 @@ resource "aws_security_group_rule" "allow_all_outbound" {
   description       = "Allow all outbound traffic"
 }
 
-resource "aws_security_group_rule" "allow_ssh_inbound" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.web_sg.id
-  description       = "Allow SSH inbound traffic"
-
+resource "aws_security_group_rule" "allow_all_from_cloud9" {
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
+  security_group_id        = aws_security_group.web_sg.id
+  source_security_group_id = aws_security_group.cloud9_sg.id
+  description              = "Allow all inbound traffic from Cloud9 SG"
 }
 
 
 resource "aws_instance" "web_server_1" {
-  ami                         = "ami-0360c520857e3138f" # Ubuntu Server 20.04 LTS (HVM), SSD Volume Type
+  ami                         = "ami-0360c520857e3138f" # Ubuntu Server 24.04 LTS (HVM), SSD Volume Type
   instance_type               = "t3.small"
   subnet_id                   = aws_subnet.public_subnet_1.id
   vpc_security_group_ids      = [aws_security_group.web_sg.id]
@@ -67,6 +66,22 @@ resource "aws_instance" "web_server_1" {
     Name = "project-webapp-web-server-1"
   }
 }
+
+resource "aws_instance" "web_server_2" {
+  ami                         = "ami-0360c520857e3138f" # Ubuntu Server 24.04 LTS (HVM), SSD Volume Type
+  instance_type               = "t3.small"
+  subnet_id                   = aws_subnet.public_subnet_1.id
+  vpc_security_group_ids      = [aws_security_group.web_sg.id]
+  associate_public_ip_address = true
+  iam_instance_profile        = "LabInstanceProfile"
+  user_data                   = file("${path.module}/scripts/UserdataScript-phase-3.sh")
+
+  tags = {
+    Name = "project-webapp-web-server-2"
+  }
+}
+
+
 
 # RDS SG
 resource "aws_security_group" "rds_sg" {
@@ -89,6 +104,16 @@ resource "aws_security_group_rule" "allow_mysql_inbound" {
   source_security_group_id = aws_security_group.web_sg.id
   description              = "Allow MySQL inbound traffic from web server SG"
 
+}
+
+resource "aws_security_group_rule" "allow_mysql_from_cloud9" {
+  type                     = "ingress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.rds_sg.id
+  source_security_group_id = aws_security_group.cloud9_sg.id
+  description              = "Allow MySQL inbound traffic from Cloud9 SG"
 }
 
 resource "aws_security_group_rule" "allow_all_outbound_rds" {
