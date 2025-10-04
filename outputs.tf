f1c911455675dec55b1fcfddfddea0b4dc1604a3
Vitor Soller <vgsoller@gmail.com>
2025-10-03 22:07:31 -0300

    add output vars

---
+++ outputs.tf
@@ -0,0 +1,14 @@
+output "rds_endpoint" {
+  description = "RDS instance endpoint (address:port)"
+  value       = aws_db_instance.aws_db_instance.endpoint
+}
+
+output "rds_port" {
+  description = "RDS instance port"
+  value       = aws_db_instance.aws_db_instance.port
+}
+
+output "rds_db_name" {
+  description = "RDS database name (db_name). If you didn't set a db_name in the aws_db_instance resource this may be empty."
+  value       = aws_db_instance.aws_db_instance.db_name
+}
