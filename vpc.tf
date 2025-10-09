bcd92817c6a7ce566adc0f63b217b74267343805
Vitor Soller <vgsoller@gmail.com>
2025-10-09 19:06:10 -0300

    fix: remove map

--- vpc.tf
+++ vpc.tf
@@ -11,10 +11,9 @@ resource "aws_vpc" "main_vpc" {
 
 
 resource "aws_subnet" "public_subnet_1" {
-  vpc_id                  = aws_vpc.main_vpc.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = "project-webapp-public-subnet-1"
@@ -32,10 +31,9 @@ resource "aws_subnet" "private_subnet_1" {
 }
 
 resource "aws_subnet" "public_subnet_2" {
-  vpc_id                  = aws_vpc.main_vpc.id
-  cidr_block              = "10.0.3.0/24"
-  availability_zone       = "us-east-1b"
-  map_public_ip_on_launch = true
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1b"
 
   tags = {
     Name = "project-webapp-public-subnet-2"
