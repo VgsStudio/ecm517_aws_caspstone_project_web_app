903e0d2e96e7891ce6cd5651d689a1cb09b5af0e
Vitor Soller <vgsoller@gmail.com>
2025-10-09 23:02:18 -0300

    add alb to output

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,13 @@ output "rds_db_name" {
   description = "RDS database name (db_name). If you didn't set a db_name in the aws_db_instance resource this may be empty."
   value       = aws_db_instance.aws_db_instance.db_name
 }
+
+output "alb_url" {
+  description = "The URL of the Application Load Balancer"
+  value       = "http://${aws_alb.web_server_2_alb.dns_name}"
+}
+
+output "alb_dns_name" {
+  description = "The DNS name of the Application Load Balancer"
+  value       = aws_alb.web_server_2_alb.dns_name
+}
